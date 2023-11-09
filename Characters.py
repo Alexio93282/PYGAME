38d16a74a4b0bc4ad12880f8ba14d1a720546c82
Alexio93282 <alexgjerstad4@gmail.com>
2023-11-09 13:29:21 +0100

    psh 3

--- Characters.py
+++ Characters.py
@@ -4,22 +4,60 @@ import random
 player_image = pg.image.load("images/VergilDMC5.png")
 enemy_image = pg.image.load("images/Dante enemy.jpg")
 ranged_image = pg.image.load("images/Ebony Ivory.jpg")
+bg_img = pg.image.load("images/BG.png")
 
 enemy_image = pg.transform.scale(enemy_image,(208,300))
 ranged_image = pg.transform.scale(ranged_image,(30,30))
+bg_img = pg.transform.scale(bg_img,(2560,1440))
+
+
+STANDING = pg.image.load('images/VergilDMC5.png')
+STANDING2 = pg.image.load('images/Idle animation.jpg')
+STANDING3 = pg.image.load('images/Idle animation2.jpg')
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, all_sprites):
+    def __init__(self, all_sprites, enemies):
         pg.sprite.Sprite.__init__(self)
+        self.current_frame = 0   
+        self.last_update = 0 
         self.image = player_image 
         self.rect = self.image.get_rect()
         self.pos_x = 0
         self.pos_y = 400
-        self.speed = random.randint(15,15)
+        self.speed = 20
+        self.hp = 1000
         self.all_sprites = all_sprites
+        self.enemies = enemies
+        self.standing = True 
+        self.standing_frames = [STANDING, STANDING2, STANDING3]
+
+        
+    def take_dmg(self, dmg):
+        self.hp -= dmg
+        if self.hp <= 0:
+            self.kill()
+    
+    def update(self):
+        # kjør animasjon funksjon i starten av update, 
+        # dette sørger for at vi animerer player for hver frame i game loopen
+         self.animate()
+        
+         self.rect.center = self.pos
+         self.standing = True
+
+    def animate(self):
+        now = pg.time.get_ticks()   # på starten av animate henter vi hvilken "tick" eller frame vi er på 1 tick er 1 FPS
+ 
+        if self.standing:   # vis vi står stille, altså dette er animasjonen vi vil kjøre om vi status for player er "standing"         
+            if now - self.last_update > 350:   # her sørger vi for at vi bytte bilde kun hver 350 tick, lavere tall animerer fortere
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+                self.image = self.standing_frames[self.current_frame]
+                self.rect = self.image.get_rect()
+
 
     def attack(self):
-        projectile = ranged_attack(self.pos_x, self.pos_y)
+        projectile = ranged_attack(self.pos_x+10, self.pos_y, self.enemies)
         print("Motivation")
         projectile.add(self.all_sprites)
 
@@ -47,8 +85,8 @@ class Enemy(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
         self.image = enemy_image
         self.rect = self.image.get_rect()
-        self.pos_x = 900
-        self.pos_y = random.randint(0,600)
+        self.pos_x = 2600
+        self.pos_y = random.randint(0,2560)
         self.speed = random.randint(1,10)
    
     def update(self):
@@ -57,15 +95,16 @@ class Enemy(pg.sprite.Sprite):
 
         self.pos_x -= self.speed
 
-        if self.pos_x > 1205:
+        if self.pos_x < 0:
             self.kill()
 
 class ranged_attack(pg.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self,x,y, enemies):
         pg.sprite.Sprite.__init__(self)
         self.image = ranged_image
         self.rect = self.image.get_rect()
         self.image.set_colorkey((255,255,255))
+        self.enemies = enemies
 
         self.pos_x = x
         self.pos_y = y
@@ -80,3 +119,5 @@ class ranged_attack(pg.sprite.Sprite):
         self.rect.y = self.pos_y
  
         self.pos_x += self.speed
+
+        hits = pg.sprite.spritecollide(self, self.enemies, True)
--- pie game.py
+++ pie game.py
@@ -12,16 +12,19 @@ GREEN =(0,255,0)
 YELLOW = (255,255,0)
 BLUE = (0,0,255)
 
-screen = pg.display.set_mode((1280,1024))
+screen = pg.display.set_mode((2560,1440))
 clock = pg.time.Clock()
+
+font_cs30 = pg.font.SysFont("Comic Sans", 30)
+font_times40 = pg.font.SysFont("Times New Roman", 40)
 
 
 all_sprites = pg.sprite.Group()
-player = Player(all_sprites)
+enemies = pg.sprite.Group()
+player = Player(all_sprites, enemies)
 enemy = Enemy()
 all_sprites.add(player)
 all_sprites.add(enemy)
-enemies = pg.sprite.Group()
 enemies.add(enemy)
 
 
@@ -52,6 +55,7 @@ while playing:
             playing = False
             pg.quit()
     
+    
     keys = pg.key.get_pressed()
     if keys[pg.K_w]:
         pos_y -= 5
@@ -66,18 +70,27 @@ while playing:
         pos_x = 1200
 
 
-    if len(all_sprites) < 10:
+    if len(enemies) < 50:
         new_enemy = Enemy()
         all_sprites.add(new_enemy)
         enemies.add(new_enemy)
 
     all_sprites.update()
+
+    hits = pg.sprite.spritecollide(player, enemies, True)
+    if hits:
+        #player.hp -= 10
+        player.take_dmg(1)
+ 
+        print(player.hp)
+ 
+        hp_text = font_times40.render(f"HP: {player.hp}", False, (RED))
     
     HJONK_HJONK = pg.sprite.spritecollide(player, enemies, True )
     if HJONK_HJONK:
         print("I am up one, Where did you learn to count we are even")
 
-    screen.fill(BLUE)
+    screen.blit(bg_img,(0,0))
     all_sprites.draw(screen)
 
     pg.display.update()
@@ -85,4 +98,4 @@ while playing:
     #pos_x += 10
     #if pos_x > 1200:
        # pos_x = -200
-       # pos_y += 10
+       # pos_y += 10 
